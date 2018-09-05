3225fef95c88a274a0ef06940910be258c3a3a24
Anushka Sehrawat <asehrawat@tavisca.com>
2018-09-05 14:22:00 +0530

    factory design pattern

---
+++ solidapproach/solidapproach/HotelProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solidapproach
+{
+    class HotelProduct:IProduct
+    {
+        public string GetTypeOfProduct()
+        {
+            return "Hotel";
+        }
+
+        public void Book()
+        {
+            Console.WriteLine("Hotel Booked");
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Hotel Product saved");
+        }
+    }
+}
---
+++ solidapproach/solidapproach/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solidapproach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string itemType;
+            Console.WriteLine("Enter the type of product you want to book:");
+            itemType = Console.ReadLine();
+          
+            gettingtype obj = new gettingtype(itemType);
+            IProduct finalObject = obj.DetermineType();
+            if (finalObject != null)
+            {
+                finalObject.GetTypeOfProduct();
+                finalObject.Book();
+                finalObject.Save();
+            }
+            else
+            {
+                Console.WriteLine("The provided type does not exists.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
